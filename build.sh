f91080ac4c25616bd3c9dc17aa257265721f0628
Matace <1293893128@qq.com>
2021-05-11 09:40:22 +0800

    scope(build.sh): add the package build

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
-if [  "$1"x = "r"x ]
+if [ "$1"x = "r"x ]
 then
     python main.py
-elif [  "$1"x = "c"x ]
+elif [ "$1"x = "c"x ]
 then
     python clear.py
+elif [ "$1"x = "b"x ]
+then
+    python setup.py sdist
+elif [ "$1"x = "rm"x ]
+then
+    rm -rf dist package_demo.egg-info
 fi
